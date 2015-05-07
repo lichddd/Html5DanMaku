57dd15271758c2916cfa8b505647a265a02d491e
木佐凪樹 <lichddd@126.com>
2015-05-07 15:09:33 +0800

    修正部分bug

--- js/Html5DanMaku.js
+++ js/Html5DanMaku.js
@@ -26,31 +26,58 @@ function ontime() {
 		d.setHours(0,0,0,0);
 		d.setMilliseconds(video.prop('currentTime')*1000);
 		$('h1').text(d.toTimeString());
-		for (var i = 0; i < danmaku.length;) {
-			if(Math.floor(video.prop('currentTime'))==danmaku[i].time)
+		
+		var temparr=new Array();
+		var i=0;
+		while (danmaku.length>0){
+			var danmakuO=danmaku[0];
+			if(Math.floor(video.prop('currentTime'))==danmakuO.time)
 			{
-				var p=$('<p></p>');
-				p.text(danmaku[i].text);
-				p.css('top','100px');
+				
+				var p;
+				p=$('<p></p>');
+				temparr.push(p);
+				p.text(danmakuO.text);
+				p.css('top',(i%25*10+10)+'px');
 				p.css('left','640px');
+				p.addClass('danmaku');
+				
 				
 
 				p.appendTo('#danmaku_continer');
-				p.addClass('danmaku');
-				var sx =setTimeout(function () {
-					
-					p.css('left','-'+p.width()+'px');
-				},1);
-				
-//				var st =setTimeout(function () {
-//					p.remove();
-//				},2000);
+				i++;
 				danmaku.shift();
 			}
 			else
 			{
 				break;
 			}
-			
 		}
+
+		
+		for (var j = 0; j < temparr.length;j++) {
+			
+				var o= temparr[j];
+				o.css('left','-'+o.width()+'px');
+				
+		}
+//		setTimeout(function () {
+//					p.css('left','-'+p.width()+'px');
+//					
+//				},1);
+				
+		setTimeout(function () {
+					for (var j = 0; j < temparr.length;j++) {
+			
+				var o= temparr[j];
+				o.remove();
+				
+		}
+				},2000);
+		
+		
+				
+
+		
+		
 	}
